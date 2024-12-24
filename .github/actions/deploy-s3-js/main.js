fa823ba08fc54501ae98d9dd71cbced05d6a6efc
unknown <vipboyxu2k3@gmail.com>
2024-12-24 20:04:00 +0700

    updated workflow

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,8 +1,19 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
+const github = require("@actions/github"); // this libary to get more extra data from github
 const exec = require("@actions/exec");
 
 const run = () => {
+  // get inputs
+  const bucket = core.getInput("bucket", { required: true });
+
+  const bucketRegion = core.getInput("bucket-region", { required: true });
+
+  const distFolder = core.getInput("dist-folder", { required: true });
+
+  // upload to s3
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
   core.notice("wellcome blyat");
 };
 
